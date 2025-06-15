9ef1d35dd10a97e299f5fb6e9bb12a5499f00299
M0H-0 <mohammmad200412@gmail.com>
2025-06-15 18:38:15 +0300

    Initial commit

---
+++ Controllers/TaksController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ToDoList.Data;
+using ToDoList.Models;
+namespace ToDoList.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly AppDbContext _context;
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var task = _context.ToDoTask.ToList();
+            return Json(task);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ToDoTask data)
+        {
+            _context.ToDoTask.Add(data);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }  
+}
---
+++ Models/ToDoTask.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models;
+public class ToDoTask{
+public int Id {get; set;}
+
+[Required]
+[StringLength (20,MinimumLength =1)]
+public string TaskName {get; set;} = string.Empty;
+
+public bool IsDone {get; set;}
+}
