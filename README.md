2bdfd1f407c862c84b7e65ce20365495cbfe283e
M0H-0 <mohammmad200412@gmail.com>
2025-06-21 07:02:29 +0300

    Updating the home view

---
+++ README.md
@@ -0,0 +1,28 @@
+# ToDoList-MVC
+
+This is a simple crud To-Do List web app I'm building while learning ASP.NET Core MVC.
+
+## About
+
+I'm using this project to practice:
+
+- ASP.NET Core MVC
+- C#
+- Entity Framework Core
+- Basic CRUD operations
+
+## Current Features
+
+- Add, veiw tasks
+- ....
+More features coming as I learn!
+
+## How to run
+
+1. Clone the repo  
+2. Run with `dotnet run`  
+3. Open the link shown in the terminal  
+
+---
+
+I’m building this project to improve my skills in web development and backend programming.
--- obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd4468d15232a8f54e379a08845bc8047bcd497b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fbf6429d9adaa2a8935f2ac2c794dd7ff475bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
